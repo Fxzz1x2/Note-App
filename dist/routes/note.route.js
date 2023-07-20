971316926dd9c58af17547222947455adb830525
Fxzz1x2 <kareemabukush@gmail.com>
2023-07-20 12:34:22 +0200

    fetch single, all notes endpoint addition

--- dist/routes/note.route.js
+++ dist/routes/note.route.js
@@ -14,5 +14,33 @@ router.post("/create-note", verifyToken, async (req, res) => {
         res.status(501).json({ error: "Failed to create Note" });
     }
 });
+router.get("/get-all-notes", verifyToken, async (req, res) => {
+    const userId = req.userId;
+    try {
+        const allNotes = await Note.findAll({
+            where: { userId },
+        });
+        res.status(200).json(allNotes);
+    }
+    catch (error) {
+        console.error("Error fetching notes:", error);
+        res.status(500).json({ error: "Failed to fetch notes." });
+    }
+});
+router.get("/get-note/:id", verifyToken, async (req, res) => {
+    const userId = req.userId;
+    const noteId = parseInt(req.params.id);
+    try {
+        const note = await Note.findOne({ where: { id: noteId, userId } });
+        if (!note) {
+            return res.status(404).json({ error: "Note not found" });
+        }
+        res.status(200).json(note);
+    }
+    catch (error) {
+        console.error("Error fetching note:", error);
+        res.status(500).json({ error: "Failed to fetch note." });
+    }
+});
 export default router;
 //# sourceMappingURL=note.route.js.map
