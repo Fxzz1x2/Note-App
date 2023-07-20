f1378e0d7490b4b8ae1bf85dc9f51d996bb16ae8
Fxzz1x2 <kareemabukush@gmail.com>
2023-07-20 11:55:16 +0200

    split code up , for better readability

--- dist/routes/user.route.js
+++ dist/routes/user.route.js
@@ -1,48 +1,7 @@
 import { Router } from "express";
-import User from "../models/user.model.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
+import { login, register } from "../auth.js";
 const router = Router();
-const generateAccessToken = (userId) => {
-    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1h" });
-};
-router.post("/register", async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({
-            username,
-            email,
-            password: hashedPassword,
-        });
-        res.status(201).json(user);
-    }
-    catch (err) {
-        console.error("Error during regisration:", err);
-        res.status(500).json({ error: "Could not create user." });
-    }
-});
-router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ where: { email } });
-        if (!user) {
-            return res.status(401).json({ error: "Invalid credentials." });
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ error: "Invalid credentials." });
-        }
-        const accessToken = generateAccessToken(user.id);
-        res.setHeader("Authorization", accessToken);
-        res.json({ user });
-    }
-    catch (error) {
-        console.error("Error during login:", error);
-        res.status(500).json({ error: "Internal server error." });
-    }
-});
+router.post("/register", register);
+router.post("/login", login);
 export default router;
 //# sourceMappingURL=user.route.js.map
---
+++ src/auth.ts
@@ -0,0 +1,56 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import User from "./models/user.model.js";
+
+dotenv.config();
+
+const generateAccessToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid credentials." });
+    }
+
+    const accessToken = generateAccessToken(user.id);
+
+    res.setHeader("Authorization", accessToken);
+
+    res.json({ user });
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ error: "Internal server error." });
+  }
+};
+
+const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json(user);
+  } catch (err) {
+    console.error("Error during registration:", err);
+    res.status(500).json({ error: "Could not create user." });
+  }
+};
+
+export { login, register };
