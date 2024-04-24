873bc949885efdd290c11c8f0e9dce5ef2830f85
Fxzz1x2 <kareemabukush@gmail.com>
2024-04-25 00:42:17 +0200

    testing git stuff rn

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,6 @@ app.use("/notes", noteRoute);
 
 syncDatabase().then(() => {
     app.listen(process.env.PORT, () =>
-        console.log(`Server is running on port: ${process.env.PORT}`)
+        console.log(`Server is running on port  ${process.env.PORT}`)
     );
 });
--- src/database.ts
+++ src/database.ts
@@ -5,28 +5,28 @@ import Logger from "./logger.js";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "mysql",
-    dialect: process.env.DB_DIALECT as Dialect,
-    port: 3306,
-  }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: "mysql",
+        dialect: process.env.DB_DIALECT as Dialect,
+        port: 3306,
+    }
 );
 
 async function syncDatabase() {
-  const logger = Logger.getInstance();
+    const logger = Logger.getInstance();
 
-  try {
-    await sequelize.authenticate();
-    logger.log("Connection has been established successfully.");
+    try {
+        await sequelize.authenticate();
+        logger.log("Connection has been established successfully");
 
-    await sequelize.sync();
-    logger.log("Models synchronized with the database.");
-  } catch (error) {
-    logger.error("Unable to connect to the database:", error);
-  }
+        await sequelize.sync();
+        logger.log("Models synchronized with the database.");
+    } catch (error) {
+        logger.error("Unable to connect to the database:", error);
+    }
 }
 
 export { syncDatabase, sequelize };
