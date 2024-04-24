4164292fc472e0cb161663adcb74ddd9338686d8
Fxzz1x2 <kareemabukush@gmail.com>
2024-04-25 00:13:37 +0200

    test

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use("/users", userRoute);
 app.use("/notes", noteRoute);
 
 syncDatabase().then(() => {
-  app.listen(process.env.PORT, () =>
-    console.log(`Server is running on port ${process.env.PORT}`)
-  );
+    app.listen(process.env.PORT, () =>
+        console.log(`Server is running on port: ${process.env.PORT}`)
+    );
 });
